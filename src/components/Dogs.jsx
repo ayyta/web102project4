0afe73a9f266c1760305e52b2f5d76de9f065cda
ALVIS YONG LAPTOP <yong.mark.alvis@gmail.com>
2023-10-13 18:48:07 -0700

    fixed discover button, need to make ban list

--- src/components/Dogs.jsx
+++ src/components/Dogs.jsx
@@ -4,13 +4,15 @@ const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 const DogInfo = (props) => {
   return (
     <>
-      <div className='check'>
+      <div className="info-container">
         {props.list.map((attribute) => (
           <button onClick={() => addToBanned(attribute)}>{attribute}</button>
         ))}
       </div>
       <p>{props.name}</p>
-      <img src={props.img}></img>
+      <div className="img-container">
+        <img src={props.img}></img>
+      </div>
     </>
   )
 }
@@ -37,44 +39,45 @@ const Dogs = () => {
   const addToBanned = (attribute) => {
     setBannedAttr([...bannedAttr, attribute]);
   }
+  const callAPI = async (query) => {
+    try {
+      const response = await fetch(query);
+      const json = await response.json();
+
+      const data = json[0];
+      const attr = data.breeds[0]
+      setCatImg(data.url);
+      setAttr([attr.life_span, attr.origin, attr.weight.imperial])
+      setName(attr.name)
+      console.log('over here bruh',data)
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    const API_KEY = "live_4oeewzmDDTUWebj93os6tROFnFYwvMZFfANYs5ZvcLAT6rd6tCLNKf582ipyzIB4";
+    let url = `https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=${API_KEY}`;
+    callAPI(url).catch(console.error);
+  }, [])
+
 
   const handleDiscoverClick = () => {
-    useEffect(() => {
-      const API_KEY = "live_4oeewzmDDTUWebj93os6tROFnFYwvMZFfANYs5ZvcLAT6rd6tCLNKf582ipyzIB4";
-      let url = `https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=${API_KEY}`;
-      let imgURL = `https://api.thecatapi.com/v1/images/search?breed_ids={catID}`
-      const callAPI = async (query) => {
-        try {
-          const response = await fetch(query);
-          const json = await response.json();
-
-          const data = json[0];
-          const attr = data.breeds[0]
-          setCatImg(data.url);
-          setAttr([attr.life_span, attr.origin, attr.weight.imperial])
-          setName(attr.name)
-          console.log('over here bruh',data)
-
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-
-      callAPI(url).catch(console.error);
-
-
-    }, []) 
+    const API_KEY = "live_4oeewzmDDTUWebj93os6tROFnFYwvMZFfANYs5ZvcLAT6rd6tCLNKf582ipyzIB4";
+    let url = `https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1&api_key=${API_KEY}`;
+    callAPI(url).catch(console.error);
   }
   
   console.log("HELLO", attributes, name, catImg)
   const sampleInfo = [['hello', 'this works'], 'name', 'https://img.freepik.com/free-photo/isolated-happy-smiling-dog-white-background-portrait-4_1562-693.jpg']
   return (
   <>
-    <div>
-      <h1>Find Your Dawg</h1>
-      <p>discover dogs</p>
+    <div className="cat-info-container">
+      <h1>Find Your Cat</h1>
+      <p>Discover New Breeds of Cats</p>
       <DogInfo list={attributes} name={name} img={catImg}/>
-      <button onClick={handleDiscoverClick}>Discover</button>
+      <button className="discover-button" onClick={handleDiscoverClick}>Discover</button>
     </div>
   </>
   )
